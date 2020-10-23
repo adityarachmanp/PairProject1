1afc36916bbd17fb2a76a34fb83b8b7afead111d
mhmdjundana <mhmd.jundana@gmail.com>
2020-10-23 08:57:13 +0700

    aasf

--- migrations/20201021095146-create-customer-product.js
+++ migrations/20201021095146-create-customer-product.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up:  (queryInterface, Sequelize) => {
-    await queryInterface.createTable('CustomerProducts', {
+    return  queryInterface.createTable('CustomerProducts', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down:  (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('CustomerProducts');
+    return  queryInterface.dropTable('CustomerProducts');
   }
 };
